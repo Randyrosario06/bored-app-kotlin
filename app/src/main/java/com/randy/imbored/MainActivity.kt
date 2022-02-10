87bb382a323d978e8a2c97aab7e5e134f6e09489
Randy <randy.rosario@iplus.do>
2022-02-10 15:17:29 -0400

    function for GET Request

--- app/src/main/java/com/randy/imbored/MainActivity.kt
+++ app/src/main/java/com/randy/imbored/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.randy.imbored
 
+import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.*
 
 class MainActivity : AppCompatActivity() {
+    var textView : TextView? = null
+     var btnGet :Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.txtWelcome)
+        btnGet= findViewById(R.id.btnStopBoring)
+        btnGet?.setOnClickListener {
+            GetActivity()
+        }
+    }
+
+    fun  GetActivity(){
+        // Instantiate the cache
+        val cache = DiskBasedCache(cacheDir, 1024 * 1024) // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        val network = BasicNetwork(HurlStack())
+
+        // Instantiate the RequestQueue with the cache and network. Start the queue.
+        val requestQueue = Volley.newRequestQueue(applicationContext)
+        val url = "https://www.boredapi.com/api/activity/"
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+                 { response ->
+                    Log.d("RESPONSE", response.toString())
+                  textView?.text = "Response: %s"+response.toString()
+                },
+                Response.ErrorListener { error ->
+                    // TODO: Handle error
+                }
+        )
+        requestQueue.add(jsonObjectRequest)
     }
 }
